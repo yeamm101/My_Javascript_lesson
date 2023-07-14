3c0ef89562c13ae2727b5a6321649c60c65d0285
yeamm101 <yeamm101@gmail.com>
2023-07-14 20:43:06 +0300

    Create HTML and JS file
    started learning Javascript by setting variables and console logging them.

---
+++ Beginner.js
@@ -0,0 +1,19 @@
+// console logging strings
+console.log("hello there");
+console.log("hello my brothers");
+
+// setting variable and console logging them
+var name = "gogoanime";
+console.log(name);
+
+var sentence =
+  "how are you doing to day? I am doing well and I hope you have a great day.";
+console.log(sentence);
+
+var food = 20;
+console.log(food);
+
+// console logging a prompt
+// a prompt is a questionary that askes a user as an alert pop-up
+var sport = prompt("What is your favorite sport?");
+console.log(sport);
