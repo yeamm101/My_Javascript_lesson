faa50317a4a2f6a057654d79897ea9851b4d41f6
yeamm101 <yeamm101@gmail.com>
2023-07-14 21:16:28 +0300

    create if_statement.js
    Wrote some some if statements and made a baby weather app.

---
+++ if_statement.js
@@ -0,0 +1,28 @@
+//user input
+/* data types
+number 22135654
+strings 'hi' or "hi"
+arrays[]
+objects{}
+booleans true/false
+*/
+
+/* math oprators
+** exponents
+% modulo(remainder)
+*/
+
+/*math methods
+Floor = round down
+ceil = round up
+random = gives you a random number between 0 and 1 so to get a nuber between 0 and 6 we multiply Math.random() by 6
+*/
+
+// baby weather app
+let weather = prompt("Is the weather sunny or rainy outside?");
+
+if (weather == "rainy") {
+  console.log("Don't forget to grab your umberella.");
+} else {
+  console.log("Wear simple cloth and don't forget your sunglasses.");
+}
